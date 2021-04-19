e197c8759daa67756c14001ee89c3d435ca413b9
Prince Vii Mosa <princevxiii.mosamoabi@programmer.net>
2021-04-19 22:29:15 +0200

    Commit not staged for commit:

---
+++ Projects/Print/print.py
@@ -0,0 +1,13 @@
+#Lesson: Print
+#Name: Mosa Moabi G
+#Date: 19/04/2021
+#Time: 21:45
+
+#to print an text written in the middle of the inverted comma's
+print("Hello World")
+
+name = input("What is your name?")
+
+print("Hello")
+print(name)
+
