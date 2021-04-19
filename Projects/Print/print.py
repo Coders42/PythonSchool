120d287c7c0f0c94b98d3a39aaa2736e1b4321f8
Prince Vii Mosa <princevxiii.mosamoabi@programmer.net>
2021-04-19 22:32:27 +0200

    Changes to be committed:

--- Projects/Print/print.py
+++ Projects/Print/print.py
@@ -2,6 +2,7 @@
 #Name: Mosa Moabi G
 #Date: 19/04/2021
 #Time: 21:45
+#Lang: Python
 
 #to print an text written in the middle of the inverted comma's
 print("Hello World")
@@ -10,4 +11,25 @@ name = input("What is your name?")
 
 print("Hello")
 print(name)
+
+# or to display on two different lines but in one code
+print("Hello, \nWorld")
+
+#Now for mathematical displayments
+print("Watch this")
+
+a = (19 * 3)
+b = a / 2
+
+print("The answer is :")
+print(b)
+
+#Then Look how it gos here
+ g = (49 * 7)
+ e = g + 140
+ f = (e / 3)
+ 
+ #Let's take a look at the answer
+ print("The answer is :")
+ print(f)
 
